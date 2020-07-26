2212a8dfc5b99c91f0f87be979e1837b13362e00
Nicolas Vega <nico_vt@protonmail.com>
2020-07-27 00:24:33 +0200

    simple intro page with pure css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,62 @@
 import React from 'react';
+import './theme';
+import {GlobalStyle, theme} from "./theme";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+    color: ${theme.colors.themePrimary};
+    padding: 10px 0 10px 0;
+    margin: 0;
+    border-bottom: 4px solid;
+    border-right: 0.5px solid;
+`
+
+const StyledP = styled.p`
+
+  color: ${theme.colors.background};
+  
+`
+const StyledDiv2 = styled.div`
+  color: ${theme.colors.background};
+`
+
+
+const LayoutLeft = styled.main`
+    max-width: 500px;
+    float: left;
+    margin-bottom: 5em;
+`
+
+const Section = styled.section`
+    display: block;
+`
+
 
 function App() {
-  return (
-    <div>
-      Hola
-    </div>
-  );
+    return (
+        <>
+            <LayoutLeft>
+                <GlobalStyle/>
+                <Section>
+                    <StyledDiv>
+                        <StyledP>Nicolás Vega Terrazas</StyledP>
+                    </StyledDiv>
+                    <StyledDiv>
+                        <StyledDiv2> is a fullstack developer at the Bundesdruckerei Gmbh in Berlin</StyledDiv2>
+                    </StyledDiv>
+                    <StyledDiv>
+                        <StyledDiv2>leading a frontend team of 3 developers</StyledDiv2>
+                    </StyledDiv>
+                    <StyledDiv>
+                        <StyledDiv2>maintaining a search-engine and a global indicators web application</StyledDiv2>
+                    </StyledDiv>
+                    <StyledDiv>
+                        <StyledDiv2>building typed npm packages in React</StyledDiv2>
+                    </StyledDiv>
+                </Section>
+            </LayoutLeft>
+        </>
+    );
 }
 
 export default App;
---
+++ src/theme.ts
@@ -0,0 +1,76 @@
+import styled, {css, createGlobalStyle} from 'styled-components'
+
+export {css, styled}; // huh?
+
+export const theme = {
+    colors: {
+        "themePrimary": "#00FF41",
+        "themeLighterAlt": "#f5fff7",
+        "themeLight": "#b3ffc6",
+        "themeTertiary": "#66ff8c",
+        "themeSecondary": "#1fff57",
+        "themeDarkAlt": "#00e639",
+        "themeDark": "#00c230",
+        "themeDarker": "#008f24",
+        "neutralDarkerAlt": "#0c0208",
+        "neutralDarker": "#0c0208",
+        "neutralDark": "#0c0207",
+        "neutralQuaternaryAlt": "#0b0207",
+        "neutralQuaternary": "#0a0206",
+        "neutralTertiaryAlt": "#0a0106",
+        "neutralTertiary": "#c8c8c8",
+        "neutralSecondary": "#d0d0d0",
+        "neutralPrimaryAlt": "#dadada",
+        "neutralPrimary": "#ffffff",
+        "neutralWhite": "#f4f4f4",
+        "backgroundWhite": "#f8f8f8",
+        "background": "#0D0208" // background
+    },
+};
+
+// border box: to exclude padding from the weight/height dimensions of a div.
+const reset = () => `
+html {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  background-color: ${theme.colors.backgroundWhite};
+
+}
+
+*, *::before, *::after {
+  box-sizing: inherit;
+}
+
+body {
+    margin: 20px 0 0 30px;
+    font-family: Sk-Modernist-Regular,monospace;
+    letter-spacing: -0.030em;
+    font-size: 1em;
+    line-height: 1.15em;
+    -webkit-font-smoothing: antialiased;
+
+}
+
+::selection {
+  background-color: ${theme.colors.themeLight};
+}
+
+a.anchor, a.anchor:hover, a.anchor:link {
+  background: none !important;
+}
+
+p {
+    display: block;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+}
+
+}
+`;
+
+export const GlobalStyle = createGlobalStyle`
+${reset()}
+`;
